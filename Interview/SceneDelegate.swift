24ab16105706b511ead2fa851239a3d2c2640224
Thyago Raphael <thyagoraphael1987@gmail.com>
2023-05-10 13:12:51 -0300

    Initial project challenge picpay

---
+++ Interview/SceneDelegate.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UINavigationController(rootViewController: ListContactsViewController())
+            self.window = window
+            window.makeKeyAndVisible()
+            
+        }
+    }
+}
+
---
+++ Interview/Scenes/ListContacts/Services/ListContactsService.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+private let apiURL = "https://run.mocky.io/v3/d26d86ec-fb82-48a7-9c73-69e2cb728070"
+
+/*
+ Json Contract
+[
+  {
+    "id": 1,
+    "name": "Shakira",
+    "photoURL": "https://api.adorable.io/avatars/285/a1.png"
+  }
+]
+*/
+
+class ListContactService {
+    func fetchContacts(completion: @escaping ([Contact]?, Error?) -> Void) {
+        guard let api = URL(string: apiURL) else {
+            return
+        }
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: api) { (data, response, error) in
+            guard let jsonData = data else {
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let decoded = try decoder.decode([Contact].self, from: jsonData)
+                
+                completion(decoded, nil)
+            } catch let error {
+                completion(nil, error)
+            }
+        }
+        
+        task.resume()
+    }
+}
