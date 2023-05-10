5269bb78255eef06ebc012868c2fe0746c1a5bed
Thyago Raphael <thyagoraphael1987@gmail.com>
2023-05-10 15:51:59 -0300

    refactor: Remove business logic from ViewController

--- Interview/Scenes/ListContact/Screen/ListContactsViewController.swift
+++ Interview/Scenes/ListContact/Screen/ListContactsViewController.swift
@@ -1,64 +1,65 @@
 import UIKit
 
-class UserIdsLegacy {
-    static let legacyIds = [10, 11, 12, 13]
+final class ListContactsViewController: UIViewController {
+    // MARK: - Properties
+    private let viewModel: ListContactsViewModel
+//    var alert: Alert?
     
-    static func isLegacy(id: Int) -> Bool {
-        return legacyIds.contains(id)
-    }
-}
-
-class ListContactsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    lazy var activity: UIActivityIndicatorView = {
+    // MARK: - Elements
+    private lazy var activity: UIActivityIndicatorView = {
         let activity = UIActivityIndicatorView()
         activity.hidesWhenStopped = true
         activity.startAnimating()
         return activity
     }()
     
-    lazy var tableView: UITableView = {
+    private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .plain)
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.rowHeight = 120
+        tableView.register(ContactCell.self, forCellReuseIdentifier: ContactCell.identifier)
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.rowHeight = 120
-        tableView.register(ContactCell.self, forCellReuseIdentifier: String(describing: ContactCell.self))
         tableView.backgroundView = activity
         tableView.tableFooterView = UIView()
         return tableView
     }()
     
-    var contacts = [ContactModel]()
-    var viewModel: ListContactsViewModel!
-    
-    init() {
+    init(viewModel: ListContactsViewModel) {
+        self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func loadView() {
-        let view = UIView()
-        view.backgroundColor = .white
-        
-        self.view = view
-    }
-    
+    // MARK: - LifeCile
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewModel = ListContactsViewModel()
-        configureViews()
-        
-        navigationController?.title = "Lista de contatos"
-        
-        loadData()
+        applyViewCode()
+        makeBiding()
+    }
+
+    // MARK: - Methods
+    private func makeBiding() {
+        viewModel.fetchData()
+        viewModel.reloadData = { [weak self] in
+            guard let self else { return }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+}
+
+// MARK: - ViewCodeImplementation
+extension ListContactsViewController: ViewCodeProtocol {
+    func buildHierarchy() {
+        view.addSubview(tableView)
     }
     
-    func configureViews() {
-        view.backgroundColor = .red
-        view.addSubview(tableView)
+    func setupConstraints() {
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
@@ -67,64 +68,60 @@ class ListContactsViewController: UIViewController, UITableViewDataSource, UITab
         ])
     }
     
-    func isLegacy(contact: ContactModel) -> Bool {
-        return UserIdsLegacy.isLegacy(id: contact.id)
+    func applyAdditionalChanges() {
+        view.backgroundColor = .white
+        navigationItem.title = "Lista de contatos"
     }
-    
+}
+
+extension ListContactsViewController: ListContactsViewModelProtocol {
+    func setupOnErrorReceived() {
+        viewModel.showServiceError = { [weak self] error in
+            guard let self else { return }
+            DispatchQueue.main.async {
+                self.showAlert(
+                    title: "Ops, ocorreu um erro",
+                    message: "\(String(describing: error?.localizedDescription))"
+                )
+            }
+        }
+    }
+}
+
+// MARK: - UITableViewDataSourceProtocol
+extension ListContactsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contacts.count
+        return viewModel.showCounter()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ContactCell.self), for: indexPath) as? ContactCell else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ContactCell.identifier, for: indexPath) as? ContactCell else {
             return UITableViewCell()
         }
         
-        let contact = contacts[indexPath.row]
-        cell.fullnameLabel.text = contact.name
-        
-        if let urlPhoto = URL(string: contact.photoURL) {
-            do {
-                let data = try Data(contentsOf: urlPhoto)
-                let image = UIImage(data: data)
-                cell.contactImage.image = image
-            } catch _ {}
-        }
-        
+        cell.selectionStyle = .none
+        cell.setupCell(from: .init(model: viewModel.getContactBased(on: indexPath)))
         return cell
     }
-    
+}
+
+// MARK: - UITableViewDelegateProtocol
+extension ListContactsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let contato = contacts[indexPath.row - 1]
+        let isLegacy = viewModel.isLegacy(indexPath: indexPath)
         
-        guard isLegacy(contact: contato) else {
-            let alert = UIAlertController(title: "Você tocou em", message: "\(contato.name)", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert, animated: true)
-            return
-        }
-        
-        let alert = UIAlertController(title: "Atenção", message:"Você tocou no contato sorteado", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        self.present(alert, animated: true)
+        showAlert(
+            title: isLegacy ? "Atenção" : "Você tocou em",
+            message: isLegacy ? "Você tocou no contato sorteado" : viewModel.getContactBased(on: indexPath).name
+        )
     }
-    
-    func loadData() {
-        viewModel.loadContacts { contacts, error in
-            DispatchQueue.main.async {
-                if let error = error {
-                    print(error)
-                    
-                    let alert = UIAlertController(title: "Ops, ocorreu um erro", message: error.localizedDescription, preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                    self.present(alert, animated: true)
-                    return
-                }
-                
-                self.contacts = contacts ?? []
-                self.tableView.reloadData()
-                self.activity.stopAnimating()
-            }
-        }
+}
+
+extension ListContactsViewController {
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let addAction = UIAlertAction(title: "Ok", style: .default)
+        alertController.addAction(addAction)
+        self.present(alertController, animated: true)
     }
 }
