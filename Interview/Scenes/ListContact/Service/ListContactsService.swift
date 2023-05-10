eb21c94fc147fc630299d05676e46e5076c4d469
Thyago Raphael <thyagoraphael1987@gmail.com>
2023-05-10 15:44:32 -0300

    refactor: Service Layer

--- Interview/Scenes/ListContact/Service/ListContactsService.swift
+++ Interview/Scenes/ListContact/Service/ListContactsService.swift
@@ -1,7 +1,5 @@
 import Foundation
 
-private let apiURL = "https://run.mocky.io/v3/d26d86ec-fb82-48a7-9c73-69e2cb728070"
-
 /*
  Json Contract
 [
@@ -14,24 +12,38 @@ private let apiURL = "https://run.mocky.io/v3/d26d86ec-fb82-48a7-9c73-69e2cb7280
 */
 
 class ListContactService {
-    func fetchContacts(completion: @escaping ([ContactModel]?, Error?) -> Void) {
+    func fetchContacts(completion: @escaping (Result<[ContactModel], NetworkError>) -> Void) {
+        let apiURL = "https://run.mocky.io/v3/d26d86ec-fb82-48a7-9c73-69e2cb728070"
+        
         guard let api = URL(string: apiURL) else {
+            completion(.failure(NetworkError.invalidURL))
             return
         }
         
         let session = URLSession.shared
         let task = session.dataTask(with: api) { (data, response, error) in
+            if let error = error {
+                completion(.failure(NetworkError.requestFailed(error: error)))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  httpResponse.statusCode == 200 else {
+                completion(.failure(NetworkError.invalidResponse))
+                return
+            }
+            
             guard let jsonData = data else {
+                completion(.failure(NetworkError.requestFailedWithData(errorData: Data())))
                 return
             }
             
             do {
                 let decoder = JSONDecoder()
-                let decoded = try decoder.decode([ContactModel].self, from: jsonData)
-                
-                completion(decoded, nil)
-            } catch let error {
-                completion(nil, error)
+                let decodedData = try decoder.decode([ContactModel].self, from: jsonData)
+                completion(.success(decodedData))
+            } catch {
+                completion(.failure(NetworkError.decodeError))
             }
         }
         
