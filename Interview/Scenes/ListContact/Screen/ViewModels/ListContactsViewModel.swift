eca4695f8ca8c1ad70038e9751ee6ad32943b3e9
Thyago Raphael <thyagoraphael1987@gmail.com>
2023-05-10 15:46:14 -0300

    refactor: ListContactsViewModel

--- Interview/Scenes/ListContact/Screen/ViewModels/ListContactsViewModel.swift
+++ Interview/Scenes/ListContact/Screen/ViewModels/ListContactsViewModel.swift
@@ -1,26 +1,43 @@
 import Foundation
 
-class ListContactsViewModel {
-    private let service = ListContactService()
+protocol ListContactsViewModelProtocol: AnyObject {
+    func setupOnErrorReceived()
+}
+
+final class ListContactsViewModel {
+    var model: [ContactModel]
+    private let service: ListContactService
     
-    private var completion: (([ContactModel]?, Error?) -> Void)?
+    var reloadData: (() -> Void)?
+    var showServiceError: ((Error?) -> Void)?
     
-    init() { }
+    init(model: [ContactModel] = [], service: ListContactService = ListContactService()) {
+        self.model = model
+        self.service = service
+    }
     
-    func loadContacts(_ completion: @escaping ([ContactModel]?, Error?) -> Void) {
-        self.completion = completion
-        service.fetchContacts { contacts, err in
-            self.handle(contacts, err)
+    func fetchData() {
+        service.fetchContacts { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let model):
+                self.model.append(contentsOf: model)
+                self.reloadData?()
+            case .failure(let error):
+                self.showServiceError?(error)
+            }
         }
     }
     
-    private func handle(_ contacts: [ContactModel]?, _ error: Error?) {
-        if let e = error {
-            completion?(nil, e)
-        }
-        
-        if let contacts = contacts {
-            completion?(contacts, nil)
-        }
+    func getContactBased(on index: IndexPath) -> ContactModel {
+        return model[index.row]
+    }
+    
+    func showCounter() -> Int {
+        return model.count
+    }
+    
+    func isLegacy(indexPath: IndexPath) -> Bool {
+        UserIdsLegacy.isLegacy(id: model[indexPath.row].id)
     }
 }
